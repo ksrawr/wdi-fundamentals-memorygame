fd9523f1ae655619eaa34fbc4b7e43dc84347209
ksrawr <kenneth.surban62@gmail.com>
2019-11-21 12:36:21 -0800

    project: functions(js) | created game logic using functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,23 +4,34 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-
-console.log("User flipped " + cardOne );
-
-
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-
-console.log("User flipped " + cardTwo );
-
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2) checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+// let cardOne = cards[0];
+
+// console.log("User flipped " + cardOne );
+
+
+// cardsInPlay.push(cardOne);
+
+// let cardTwo = cards[2];
+
+// console.log("User flipped " + cardTwo );
+
+// cardsInPlay.push(cardTwo);
+
+// if(cardsInPlay.length === 2) {
