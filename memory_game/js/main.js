ea978e923bedf944935b53647c06befd91978921
ksrawr <kenneth.surban62@gmail.com>
2019-11-20 20:08:56 -0800

    project: control flow(js) | created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 // console.log("Up and runninng");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
 
 console.log("User flipped " + cardOne );
-console.log("User flipped " + cardThree );
+
+
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[2];
+
+console.log("User flipped " + cardTwo );
+
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
